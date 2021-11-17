8619e19a34b74977193fa5326ad558969bf0c5fd
Paweł Kotiuk <kotiuk@zohomail.eu>
2021-11-17 23:07:34 +0100

    Add more Models to admin site

--- sjopinie/sjopinie_app/admin.py
+++ sjopinie/sjopinie_app/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Subject
+from .models import Lecturer, Opinion, Subject, Tag
 
+admin.site.register(Lecturer)
+admin.site.register(Opinion)
 admin.site.register(Subject)
+admin.site.register(Tag)
