c0553fd437a038839d42ed10a0b2892e1c14ebfd
Paweł Kotiuk <kotiuk@zohomail.eu>
2021-12-07 12:37:30 +0100

    Change site language to polish

--- sjopinie/sjopinie/settings.py
+++ sjopinie/sjopinie/settings.py
@@ -129,7 +129,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pl-PL'
 
 TIME_ZONE = 'UTC'
 
