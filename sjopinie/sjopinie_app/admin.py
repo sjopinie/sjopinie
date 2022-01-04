0c5c60ff76769296352f8bbf005e54d1dc034bd6
Paweł Kotiuk <kotiuk@zohomail.eu>
2022-01-04 17:40:22 +0100

    Add proper admin panels for Lecturers Subjects and Opinions

--- sjopinie/sjopinie_app/admin.py
+++ sjopinie/sjopinie_app/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
 from .models import Lecturer, Opinion, Subject, Tag
 
-admin.site.register(Lecturer)
-admin.site.register(Opinion)
-admin.site.register(Subject)
+
+class LecturerAdmin(admin.ModelAdmin):
+    search_fields = ['full_name']
+
+
+class SubjectAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+class OpinionAdmin(admin.ModelAdmin):
+    search_fields = ['opinion_text', 'author__username']
+    list_display = [
+        'author', 'publish_time', 'subject_of_opinion', 'lecturer_of_opinion',
+        'note_interesting', 'note_easy', 'note_useful', 'opinion_text'
+    ]
+
+
+admin.site.register(Lecturer, LecturerAdmin)
+admin.site.register(Opinion, OpinionAdmin)
+admin.site.register(Subject, SubjectAdmin)
 admin.site.register(Tag)
