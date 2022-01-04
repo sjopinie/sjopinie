ab5fa4f40f153754e6ac6cc227c9046b2d014238
Paweł Kotiuk <kotiuk@zohomail.eu>
2022-01-04 17:24:42 +0100

    Add proper panel for handling orgusers by Admin

--- sjopinie/shared/admin.py
+++ sjopinie/shared/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import OrgUser
 
-admin.site.register(OrgUser)
+
+class OrgUserAdmin(admin.ModelAdmin):
+    search_fields = ['username', 'email']
+    list_display = ['username', 'tenant', 'date_joined']
+    list_filter = ['tenant']
+
+
+admin.site.register(OrgUser, OrgUserAdmin)
