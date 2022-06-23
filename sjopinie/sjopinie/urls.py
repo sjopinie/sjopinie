6e73d94e68b0708ce04e9a5c396210556b2fad8b
Paweł Kotiuk <kotiuk@zohomail.eu>
2022-06-23 16:34:36 +0200

    Add REST API documentation site

--- sjopinie/sjopinie/urls.py
+++ sjopinie/sjopinie/urls.py
@@ -13,13 +13,30 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
-
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import path, include
+from django.views.generic import TemplateView
+
+from rest_framework.schemas import get_schema_view
+
+docs_urls = [
+    path('openapi',
+         get_schema_view(title="Sjopinie schema",
+                         description="API for all things …"),
+         name='openapi-schema'),
+    path('docs/',
+         TemplateView.as_view(template_name='docs.html',
+                              extra_context={'schema_url': 'openapi-schema'}),
+         name='docs'),
+]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('sjopinie_app.urls')),
 ]
 urlpatterns += staticfiles_urlpatterns()
+
+if settings.DEBUG:
+    urlpatterns += docs_urls
