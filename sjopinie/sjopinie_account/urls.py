c916e196cb3968c67e52e44ad118b70776c8d1b7
Paweł Kotiuk <kotiuk@zohomail.eu>
2022-06-25 16:52:49 +0200

    Don't use default endpoint for managing emails

--- sjopinie/sjopinie_account/urls.py
+++ sjopinie/sjopinie_account/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 from allauth.account.urls import urlpatterns as account_urlpatterns
 
+email_endpoint = [x for x in account_urlpatterns if x.name == "account_email"]
+account_urlpatterns.remove(email_endpoint[0])
+
 urlpatterns = [
     path('settings/', views.settings_page, name='account_settings'),
 ] + account_urlpatterns
