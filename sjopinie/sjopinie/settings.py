15e035f76aa6d40cf871326bb237f402dcba68ba
Paweł Kotiuk <kotiuk@zohomail.eu>
2022-07-23 13:40:02 +0200

    Allow manual setting static files location

--- sjopinie/sjopinie/settings.py
+++ sjopinie/sjopinie/settings.py
@@ -190,7 +190,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "/sjopinie_app/static")
+STATIC_ROOT = os.environ.get("STATIC_ROOT",
+                             os.path.join(BASE_DIR, "/sjopinie_app/static"))
 
 # Others
 
